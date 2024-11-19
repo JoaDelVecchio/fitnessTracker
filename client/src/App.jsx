f6704f5a130ba53e2d96a24ac4423a0ce72bd6e1
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-11-19 14:30:17 -0300

    Stablish client request

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div>
-      <p>HI</p>
+  const [listOfUsers, setListOfUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:3001/getUsers");
+        setListOfUsers(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching the users", error);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return loading ? (
+    <p>Loading...</p>
+  ) : (
+    <div className="App">
+      <div className="usersDisplay">
+        {listOfUsers.map((user, i) => (
+          <p key={i}>{user.name}</p>
+        ))}
+      </div>
     </div>
   );
 }
